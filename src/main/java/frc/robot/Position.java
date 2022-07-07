49bf8922a15428958c1248eb6c4935c878c47538
Will D <44533697+Grig84@users.noreply.github.com>
2022-07-07 22:02:31 +0000

    Gremlin Attack

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -5,21 +5,42 @@ import static lib.Romi.turnDegrees;
 
 public class Position {
 
+  int x = 0;
+  int y = 0;
+  char d = N;
+
 
     public void driveUp() throws InterruptedException {
-
+      driveDistance(10);
+      x = x+10;
     }
 
     public void driveDown() throws InterruptedException {
-
+      driveDistance(-10);
+      x = x-10;
     }
 
     public void driveRight() throws InterruptedException {
-
+      if (d == N) {
+        d = W;
+        x = x+10;
+      } elseif (d == E) {
+        d = N;
+        y = y+10;
+      } elseif (d == S){
+        d = E;
+        x = x-10;
+      } else {
+        d = S;
+        y = y-10;
+      }
+      turnDegrees(90);
+      driveDistance(10);
     }
 
     public void driveLeft() throws InterruptedException {
-
+      turnDegrees(-90);
+      driveDistance(10);
     }
 
     public void returnToHome() {
@@ -27,6 +48,6 @@ public class Position {
     }
 
     public String toString() {
-        return "";
+        return "(x, y)";
     }
 }
